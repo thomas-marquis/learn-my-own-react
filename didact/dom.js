fe36e51c0d09b38258ebe3eb3e1c782d43c7e691
Thomas Marquis <thomas.marquis@ext.cdiscount.com>
2020-01-21 13:35:57 +0100

    fix: handle events

--- didact/dom.js
+++ didact/dom.js
@@ -1,26 +1,63 @@
+/**
+ *
+ * @param {string} name
+ * @returns {boolean}
+ */
+const isProperty = name => name !== 'children'
+/**
+ *
+ * @param {string} name
+ * @returns {boolean}
+ */
+const isEvent = name => name.startsWith('on')
+/**
+ *
+ * @param {string} name
+ * @returns {string}
+ */
+const getEventName = name => name.toLowerCase().substring(2)
+
+/**
+ *
+ * @param {string} type
+ * @param {{}} props
+ * @returns {Text | Node}
+ */
 export function createDom({type, props}) {
   const dom = type === 'TEXT_ELEMENT' ?
     document.createTextNode(props.nodeValue)
     : document.createElement(type)
 
   Object.keys(props).forEach(name => {
-    if (name !== 'children') {
-      dom[name] = props[name]
+    if (isProperty(name)) {
+      if (isEvent(name)) {
+        dom.addEventListener(getEventName(name), props[name])
+      } else {
+        dom[name] = props[name]
+      }
     }
   })
 
   return dom
 }
 
-const isProperty = name => name !== 'children'
-
+/**
+ *
+ * @param {Node} dom
+ * @param {{}} prevProps
+ * @param {{}} nextProps
+ */
 export function updateDom(dom, prevProps, nextProps) {
   // supprime les anciennes props
   Object.keys(prevProps)
     .filter(isProperty)
     .forEach(name => {
       if(!(name in nextProps)) {
-        dom[name] = ''
+        if (isEvent(name)) {
+          dom.removeEventListener(getEventName(name), prevProps[name])
+        } else {
+          dom[name] = ''
+        }
       }
     })
 
@@ -29,7 +66,14 @@ export function updateDom(dom, prevProps, nextProps) {
     .filter(isProperty)
     .forEach(name => {
       if (prevProps[name] !== nextProps[name]) {
-        dom[name] = nextProps[name]
+        if (isEvent(name)) {
+          if (prevProps[name]) {
+            dom.removeEventListener(getEventName(name), prevProps[name])
+          }
+          dom.addEventListener(getEventName(name), nextProps[name])
+        } else {
+          dom[name] = nextProps[name]
+        }
       }
     })
 }
